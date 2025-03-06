ebf179828e402be6e826001294488170a75f5a05
Valnushka <vkonson@gmail.com>
2025-03-06 21:56:16 +0300

    Work with error pages

--- SimpleOrder/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleOrder.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
